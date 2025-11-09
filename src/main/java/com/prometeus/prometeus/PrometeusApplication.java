2b6350b2471ed8fafffb2a163d51480511cc0e25
chris00aaron <aaron17650@gmail.com>
2025-11-08 21:51:26 -0500

    Commit inicial del proyecto

---
+++ src/main/java/com/prometeus/prometeus/PrometeusApplication.java
@@ -0,0 +1,13 @@
+package com.prometeus.prometeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometeusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometeusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prometeus/prometeus/PrometeusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prometeus.prometeus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometeusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
